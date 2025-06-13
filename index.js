62abcdd1de3217c4af7bdceef3dd63e93e21c767
Debra <debrawangeci@gmail.com>
2025-06-13 17:36:36 +0300

    Define divide, square, and add functions for test passing

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. Function expression for divide
+const divide = function () {
+  return 2000 / 100;
+};
+
+// 2. Arrow function for square
+const square = (x) => x * x;
+
+// 3. Arrow function for add
+const add = (a, b) => a + b;
